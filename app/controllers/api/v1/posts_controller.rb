d37e01aee03be38149dd8d901425b2c0e3b95f23
Hang Bin Aw <awhangbin@gmail.com>
2023-01-07 20:39:28 +0800

    defined post api methods

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,36 @@
+class Api::V1::PostsController < ApplicationController
+  before_action :set_post, only: %i[show destroy]
+
+  def index
+    post = Post.all.order(created_at: :desc)
+    render json: post
+  end
+
+  def create
+    post = Post.create!(post_params)
+    if post
+      render json: post
+    else
+      render json: post.errors
+    end
+  end
+
+  def show
+    render json: @post
+  end
+
+  def destroy
+    @post&.destroy
+    render json: { message: 'Thread deleted!' }
+  end
+
+  private
+
+  def post_params
+    params.permit(:name, :image, :ingredients, :instruction)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+end

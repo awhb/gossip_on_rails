93a2675b45e71f84272b931533adb70a86697053
Hang Bin Aw <awhangbin@gmail.com>
2023-01-18 02:25:27 +0800

    added authentication

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,26 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :set_user, only: %i[show destroy]
+  before_action :authorize_request, except: :create
+  before_action :set_user, only: %i[show]
+
+  def index
+    users = User.all.order(created_at: :desc)
+    render json: users
+  end
+
+  def search
+    users = User.where("username = ?", params[:username])
+    render json: users
+  end
 
   def create
     user = User.create!(user_params)
-    if user
-      render json: user
+    if user.save
+      login()
     else
-      render json: user.errors
+      render json: {error: 'An error has occurred.'}, status: :unprocessable_entity
+    end
+    rescue ActiveRecord::RecordNotUnique
+      render json: {error: 'The username is already taken.'}, status: :unprocessable_entity
     end
   end
 
@@ -14,15 +28,20 @@ class Api::V1::UsersController < ApplicationController
     render json: @user
   end
 
-  def destroy
-    @user&.destroy
-    render json: { message: 'User deleted!' }
+  def login
+    user = User.find_by(name: params[:username])
+    if user&.authenticate(params[:password])
+        token = JsonWebToken.encode(user_id: @user.id)
+        render json: {token: token, username: user.username}
+    else
+        render json: { error: 'Invalid username or password' }, status: :unauthorized
+    end
   end
 
   private
 
   def user_params
-    params.permit(:username, :password)
+    params.require(:user).permit(:username, :password)
   end
 
   def set_user
